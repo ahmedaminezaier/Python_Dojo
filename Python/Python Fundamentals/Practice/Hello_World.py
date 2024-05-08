5bf2c4b9b7327baefbb43ea1547a543f1c1a05fb
ahmedaminezaier <zaierahmed200@gmail.com>
2024-05-08 17:45:50 +0100

    essage

--- Python/Python Fundamentals/Practice/Hello_World.py
+++ Python/Python Fundamentals/Practice/Hello_World.py
@@ -1,1 +1,12 @@
 print("Hello World")
+name= "Noelle"
+print ("Hello",name)
+print ("Hello"+name)
+name= 42
+print("Hello",name)
+print("Hello"+str(name))
+fave_food1 ="sushi"
+fave_food2 ="pizza"
+print("I love to eat sushi and pizza")
+print("I love to eat",fave_food1,"and",fave_food2)
+print("I love to eat"+fave_food1,"and"+fave_food2)
