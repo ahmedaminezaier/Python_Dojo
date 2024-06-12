c05b71ff75119af15a07d469280cd604d97f0a57
ahmedaminezaier <zaierahmed200@gmail.com>
2024-06-12 14:08:42 +0100

    message

--- Python/Flask_MySQL/Core/USER CR/flask_app/controllers/users.py
+++ Python/Flask_MySQL/Core/USER CR/flask_app/controllers/users.py
@@ -1,8 +1,6 @@
 from flask_app import app
 
-
-from flask import render_template , redirect , request 
-
+from flask import render_template , redirect , request
 
 from flask_app.models.user import User
 
